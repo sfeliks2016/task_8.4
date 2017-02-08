7afe52d3a5d69a46fc8ec2f54aa4ee051d9a0f3e
Szymon Feliks <sz.feliks@gmail.com>
2017-02-08 11:53:45 +0100

    add js/

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// plik scripts.js
+
+var a = 8,
+    h = 11,
+    triangleArea = a * h / 2;
+
+console.log('pole trojkąta o podstawie a: ' + a + ' i wysokosci h: ' + h + ' wynosi: ' + triangleArea);
+console.log(triangleArea);
